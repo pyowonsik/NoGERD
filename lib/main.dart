b94dfc8b34d45ae805feff71f0b1b1d8c37f2654
pyowonsik <94816219+pyowonsik@users.noreply.github.com>
2025-04-28 21:02:14 +0900

    refactor : 얼림 시간 수정

--- lib/main.dart
+++ lib/main.dart
@@ -82,7 +82,7 @@ Future<void> scheduleDailyNotification() async {
 tz.TZDateTime _nextInstanceOfOneThirtyPM() {
   final tz.TZDateTime now = tz.TZDateTime.now(tz.local);
   tz.TZDateTime scheduledDate =
-      tz.TZDateTime(tz.local, now.year, now.month, now.day, 13, 55);
+      tz.TZDateTime(tz.local, now.year, now.month, now.day, 21, 00);
   if (scheduledDate.isBefore(now)) {
     scheduledDate = scheduledDate.add(const Duration(days: 1));
   }
